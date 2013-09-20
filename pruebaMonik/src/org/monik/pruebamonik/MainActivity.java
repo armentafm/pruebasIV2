955383ceb3d83296a9b242f25c39c21754b2646c
Monica Fernandez <mfernandez@ohkasystems.com>
2013-09-20 18:59:50 -0500

    Agregar funcionalidad
    Ya prueba hasta la lista

--- pruebaMonik/src/org/monik/pruebamonik/MainActivity.java
+++ pruebaMonik/src/org/monik/pruebamonik/MainActivity.java
@@ -8,21 +8,33 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-import com.ohka.interesse.interesselib.model.LoginRequest;
-import com.ohka.interesse.interesselib.model.LoginResponse;
+
+import com.ohka.interesse.interesselib.model.request.BeneficiariosRequest;
+import com.ohka.interesse.interesselib.model.request.LoginRequest;
+import com.ohka.interesse.interesselib.model.request.PhoneRequest;
+import com.ohka.interesse.interesselib.model.response.LoginResponse;
+import com.ohka.interesse.interesselib.model.response.PhoneResponse;
 import com.ohka.interesse.interesselib.view.ViewNotification;
+import com.ohka.interesse.interesselib.ws.BeneficiariosClient;
 import com.ohka.interesse.interesselib.ws.LoginClient;
+import com.ohka.interesse.interesselib.ws.PhoneClient;
 
 public class MainActivity extends Activity implements ViewNotification {
 	Button button;
+	Button button_phone;
+	Button button_beneficiarios;
+	Context mcontexto;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		addListenerOnButton();
+		mcontexto = getApplicationContext();
 		
+		addListenerOnButton();
+		addListenerOnButtonPhone();
+		addListenerOnButtonBeneficiarios();
 	}
 
 	@Override
@@ -34,33 +46,81 @@ public class MainActivity extends Activity implements ViewNotification {
 	
 	
 	LoginClient lc  = new LoginClient();
-	 public void addListenerOnButton() {
-    	 lc.setController(this);
+	PhoneClient pc = new PhoneClient();
+	BeneficiariosClient bc = new BeneficiariosClient();	
+	
+	public void addListenerOnButton() {
+    	 	lc.setController(this);
 			button = (Button) findViewById(R.id.button1);			
 			 button.setOnClickListener(new View.OnClickListener() {
 	             public void onClick(View v) {
-	            	 Context context = getApplicationContext();
-	            	 System.out.println("Inicia....");
+	            	System.out.println("Inicia....");
 	         		LoginRequest rq = new LoginRequest();
-	         		//rq.setPassword("minovio188");
-	         		//rq.setUser("ysuarez@ohkasystems.com");
-	         		
-	         		rq.setPassword("1751d49d00");
+	         		rq.setPassword("1751d49d");
 	         	    rq.setUser("eframirez@ohkasystems.com");
-	         		lc.login(rq, context);
+	         		lc.login(rq, mcontexto);
+	         		
+	         			         		
 	             }
 	         });
 			
 		}
 
+	public void addListenerOnButtonPhone() {
+		pc.setController(this);
+		button = (Button) findViewById(R.id.button2);			
+		 button.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+            	System.out.println("Buscando Telefonos....");
+         		PhoneRequest pr = new PhoneRequest();
+         		pr.setId_aseguradora("13");
+         		pc.insurer_phone(pr, mcontexto);
+         	         			         	
+             }
+         });
+		
+	}
+	
+	public void addListenerOnButtonBeneficiarios() {
+		bc.setController(this);
+		button = (Button) findViewById(R.id.button3);			
+		 button.setOnClickListener(new View.OnClickListener() {
+             public void onClick(View v) {
+            	System.out.println("Buscando Beneficiarios....");
+         		BeneficiariosRequest pr = new BeneficiariosRequest();
+         		pr.setUser("113163");
+         		bc.getBeneficiarios(pr, mcontexto);         	         			         	
+             }
+         });
+		
+	}
 
 
 	@Override
 	public void receivedData(Object r) {
 		// TODO Auto-generated method stub
 		// Aquí tendría que recibir el VO de Salida de la llamada al WS 
-		LoginResponse respuesta = (LoginResponse)r;
-		Log.d("HASTA VIEW","wiii por fin xD "+respuesta.getMensaje());
+		
+		if(r instanceof LoginResponse){
+			LoginResponse respuesta = (LoginResponse)r;
+			Log.d("HASTA VIEW","wiii por fin xD "+respuesta.getNombre());
+		}else if(r instanceof PhoneResponse){
+			PhoneResponse respuesta = (PhoneResponse)r;
+			Log.d("HASTA VIEW","otra veeez xD "+respuesta.getAseguradora());
+		}
+		
+	}
+
+	@Override
+	public void receivedDataError(Object r) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void receivedDataServerError(Object r) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
